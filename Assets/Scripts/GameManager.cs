5f88532b146a56563c6d118498d4e9179156c475
Chris Pioli <christopher.pioli@gmail.com>
2019-06-30 13:02:49 -0700

    Pause Events have been added.
    GameManager.cs: will control the PlayStateMachine (to be developed) and raise all play related events such as pause, unpause, levelstart, etc.
    
    PauseScreenBehaviour.cs: exclusively handles the Paused display. Can be generalized into "MenuDisplayBehaviour"

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour {
+
+    private bool gamePaused;
+
+    public UnityEvent PauseEvent;
+    public UnityEvent ResumeEvent;
+
+	// Use this for initialization
+	void Start () {
+        gamePaused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleAllInput();
+	}
+
+    private void HandleAllInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (gamePaused)
+                ResumeEvent.Invoke();
+            else
+                PauseEvent.Invoke();
+            gamePaused = !gamePaused;
+        }
+    }
+}
