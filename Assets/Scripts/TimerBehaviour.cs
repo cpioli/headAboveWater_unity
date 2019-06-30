03e6405ffeb44256815d1e42cfbfe518cc30030b
Chris Pioli <christopher.pioli@gmail.com>
2019-06-29 17:34:01 -0700

    Added Timer
    1. Timer added to game. Begins at play, halts when program exits.
    2. Events involving Play/Stop and Pause/Resume have not been implemented yet. Those will change Timer's behavior.
    3. A minor bug: if seconds < 10, then the tens' digits 0 will not display. I doubt it's an easy fix.

---
+++ Assets/Scripts/TimerBehaviour.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+
+public class TimerBehaviour : MonoBehaviour {
+
+    private StringBuilder sb; //requires the "using System.Text" declaration
+    private Text textObject;
+    private float totalTimePassed;
+    private float minutes;
+    private float seconds;
+    private bool paused;
+
+	// Use this for initialization
+	void Start () {
+        sb = new StringBuilder();
+        textObject = gameObject.GetComponent<Text>();
+        totalTimePassed = 0.0f;
+        minutes = 0.0f;
+        seconds = 0.0f;
+        paused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (paused) return;
+        ConvertToTime();
+	}
+
+    private void ConvertToTime()
+    {
+        totalTimePassed += Time.deltaTime;
+        sb.Remove(0, sb.Length); //flush the StringBuilder
+        minutes = totalTimePassed / 60.0f;
+        minutes = Mathf.Clamp(minutes, 0.0f, 9.0f);
+        if (seconds > 6000.0f) //over ten minutes have passed
+            seconds = 59.99f;
+        else
+            seconds = totalTimePassed % 60.0f;
+        sb.Append(minutes.ToString("N0") + ":" + seconds.ToString("N2"));
+        textObject.text = sb.ToString();
+    }
+
+    //Listens to PauseEvent
+    public void Pause()
+    {
+        paused = true;
+    }
+
+    //Listens to UnpauseEvent
+    public void Resume()
+    {
+        paused = false;
+    }
+
+    //Listens to GameOverEvent
+    public void Stop()
+    {
+        paused = true;
+    }
+
+    //Listens to LevelStartEvent
+    public void Play()
+    {
+        paused = false;
+    }
+
+    //Listens to RestartLevelEvent
+    public void Reset()
+    {
+        totalTimePassed = 0.0f;
+    }
+}
+    
