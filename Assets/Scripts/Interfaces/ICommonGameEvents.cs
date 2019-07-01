128a21bd123989cb2eb9fd27ce6a9252ae6dda9e
Chris Pioli <christopher.pioli@gmail.com>
2019-07-01 15:49:12 -0700

    Adding CommonGameEvent interface
    Reduces number of GameEventListeners in the Inspector by letting the Behaviour object implement methods the root GO can find.
    
    Not implemented in any classes yet.

---
+++ Assets/Scripts/Interfaces/ICommonGameEvents.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This interface allows a prefab's root GameObject to find all child
+/// GameObjects and notify them of common game events such as when the game is
+/// paused, when the player starts a level, etc. That's at least 4 - 6 GELs 
+/// removed from each child GameObject
+/// </summary>
+/// 
+public interface ICommonGameEvents {
+    void GamePaused(bool paused);
+    void GameOver();
+    void LevelStarted();
+    void LevelCompleted();
+}
