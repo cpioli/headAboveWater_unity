9d79e01046a0e69c6cd7b5ea625c3862494485d9
Chris Pioli <christopher.pioli@gmail.com>
2019-09-06 14:24:48 -0700

    Replacing GameEventListeners with MonoBehaviour for GameOver Menu
    1. Added the CommonGELL into the Screen
    
    2. Replaced individual GameObjects' GELs in GameOverCanvas's menu with the GameOverMenuBehaviour in the parent GO, which implements ICommonGameEvents.

---
+++ Assets/Scripts/UI/GameOverMenuBehaviour.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using cpioli.Events;
+using UnityEngine.UI;
+
+public class GameOverMenuBehaviour : MonoBehaviour, ICommonGameEvents {
+
+    private Button[] menuButtons;
+    private Text[] menuTexts;
+    private Image[] menuImages;
+
+    void Awake()
+    {
+        menuButtons = GetComponentsInChildren<Button>();
+        menuTexts = GetComponentsInChildren<Text>();
+        menuImages = GetComponentsInChildren<Image>();
+        Hide();
+    }
+
+    private void Hide()
+    {
+        foreach (Button b in menuButtons)
+        {
+            b.enabled = false;
+        }
+        foreach (Text t in menuTexts)
+        {
+            t.enabled = false;
+        }
+        foreach (Image i in menuImages)
+        {
+            i.enabled = false;
+        }
+    }
+
+    private void Show()
+    {
+        foreach (Button b in menuButtons)
+        {
+            b.enabled = true;
+        }
+        foreach (Text t in menuTexts)
+        {
+            t.enabled = true;
+        }
+        foreach (Image i in menuImages)
+        {
+            i.enabled = true;
+        }
+    }
+
+    public void GameOver()
+    {
+        Show();
+    }
+
+    public void GamePaused()
+    {
+    }
+
+    public void GameResumed()
+    {
+    }
+
+    public void LevelCompleted()
+    {
+    }
+
+    public void LevelStarted()
+    {
+        Hide();
+    }
+}
