8ef05867b73705424e60cfb1aca6716f22b500c4
Chris Pioli <christopher.pioli@gmail.com>
2019-09-06 14:29:07 -0700

    PauseMenu Updated for use with GELL
    All GELs in PauseMenu are removed.

---
+++ Assets/Scripts/UI/PauseMenuBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using cpioli.Events;
+
+public class PauseMenuBehaviour : MonoBehaviour, ICommonGameEvents {
+
+    private Text text;
+
+    void Awake()
+    {
+        text = GetComponentInChildren<Text>();
+        text.enabled = false;
+    }
+
+    public void GamePaused()
+    {
+        text.enabled = true;
+    }
+
+    public void GameResumed()
+    {
+        text.enabled = false;
+    }
+
+    public void GameOver()
+    {
+    }
+
+    public void LevelStarted()
+    {
+    }
+
+    public void LevelCompleted()
+    {
+    }
+
+}
