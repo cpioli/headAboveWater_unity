22463da4e317369a06ad8d34aa83ad08d8bb2cc5
Chris Pioli <christopher.pioli@gmail.com>
2019-09-24 14:44:34 -0700

    Adding pluggable Animator commands
    The list of instructions I want to use when I go fully pluggable with my tool sets.

---
+++ Assets/Scripts/ScriptableObjects/Instructions/AnimatorUpdate.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AnimatorUpdate : ScriptableObject {
+
+    public string parameterName;
+    public abstract void UpdateAnimator(PlayerPlatformController ppc);
+}
+
+[CreateAssetMenu(menuName = "Instructions/UpdateAnimator/Bool",order = 0)]
+public class UpdateAnimatorBool : AnimatorUpdate
+{
+    public bool parameterValue;
+    public override void UpdateAnimator(PlayerPlatformController ppc) 
+    {
+        ppc.animator.SetBool(parameterName, parameterValue);
+    }
+}
+
+[CreateAssetMenu(menuName = "Instructions/UpdateAnimator/Float", order = 1)]
+public class UpdateAnimatorFloat : AnimatorUpdate
+{
+    public float parameterValue;
+    public override void UpdateAnimator(PlayerPlatformController ppc)
+    {
+        ppc.animator.SetFloat(parameterName, parameterValue);
+    }
+}
+
+[CreateAssetMenu(menuName = "Instructions/UpdateAnimator/Integer", order = 2)]
+public class UpdateAnimatorInteger : AnimatorUpdate
+{
+    public int parameterValue;
+    public override void UpdateAnimator(PlayerPlatformController ppc)
+    {
+        ppc.animator.SetInteger(parameterName, parameterValue);
+    }
+}
+
+[CreateAssetMenu(menuName = "Instructions/UpdateAnimator/Trigger", order = 3)]
+public class UpdateAnimatorTrigger : AnimatorUpdate
+{
+    public override void UpdateAnimator(PlayerPlatformController ppc)
+    {
+        ppc.animator.SetTrigger(parameterName);
+    }
+}
