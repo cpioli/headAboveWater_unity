b7f0ffa0420a4bce89d7e7d568c8f56e495bfb04
Chris Pioli <christopher.pioli@gmail.com>
2019-10-01 14:56:32 -0700

    Adding GameLevelStartState
    A new state has been added so its OnStateEnter method can restart the level, and when it exits the player will be able to play.

---
+++ Assets/Scripts/ScriptableObjects/StateSystems/GameStates/GameLevelStartState.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace cpioli.States
+{
+    public class GameLevelStartState : GameState
+    {
+        public override void OnStateEnter(GameManager gm)
+        {
+            base.OnStateEnter(gm);
+        }
+
+        public override void OnStateExit()
+        {
+            base.OnStateExit();
+        }
+
+        public override void Act()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
