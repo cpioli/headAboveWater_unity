65e113a1f2a7e936a348889a11e03d1ed3468865
Chris Pioli <christopher.pioli@gmail.com>
2019-10-15 16:02:33 -0700

    Fixed Standalone Build Mode
    1. TouchInputMenuBehaviour makes the buttons disappear.
    2. The parent Canvas's Scaler now scales with screen size (1920 x 1280)

---
+++ Assets/TouchInputMenuBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchInputMenuBehaviour : MonoBehaviour {
+
+#if UNITY_STANDALONE
+    void Awake () {
+        this.gameObject.SetActive(false);
+	}
+#endif
+}
